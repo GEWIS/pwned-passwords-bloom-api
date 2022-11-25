2b6caa249eebfe73bca8c3ba2329559dabc48086
Tom Udding <tomudding@users.noreply.github.com>
2022-11-25 20:33:51 +0100

    Add API using Rocket
    Adds the API that can be used to determine whether a hash is in the
    Redis bloom filter or not.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use rocket_db_pools::{Connection, Database};
+use rocket_db_pools::deadpool_redis::Pool;
+use rocket_db_pools::deadpool_redis::redis::cmd;
+use rocket::{build, get, main, routes};
+use rocket::request::FromParam;
+
+#[derive(Database)]
+#[database("redis")]
+struct PwnedPasswords(Pool);
+
+struct Hash<'r> {
+    value: &'r str,
+}
+
+impl<'r> FromParam<'r> for Hash<'r> {
+    type Error = &'r str;
+
+    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
+        if 40 != param.chars().count() {
+            return Err("Hash is not expected length.");
+        }
+
+        if !param.chars().all(|c| (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+            return Err("Hash is not valid.");
+        }
+
+        Ok(Hash { value: param })
+    }
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/<hash>")]
+async fn pwned(mut redis: Connection<PwnedPasswords>, hash: Hash<'_>) -> String {
+    let value: bool = cmd("BF.EXISTS")
+        .arg("pwned-bloom")
+        .arg(hash.value)
+        .query_async(&mut *redis)
+        .await.unwrap();
+
+    format!("Hello, {}!", value)
+}
+
+#[main]
+async fn main() {
+    let _ = build().attach(PwnedPasswords::init()).mount("/", routes![index, pwned]).launch().await;
 }
